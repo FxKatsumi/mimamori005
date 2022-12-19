6077f4a4fd1557e964f3befff62b969f85079678
fxKatsumi <takahata@forex.co.jp>
2022-12-19 14:28:09 +0900

    改修

--- app.py
+++ app.py
@@ -143,76 +143,97 @@ net = None
 
 # メール送信
 def sendMail(mail_to, subject, msg):
-    msg = MIMEText(msg, "plain", "utf-8")
-    msg["Subject"] = subject
-    msg["From"] = mail_from
-    msg["To"] = mail_to
 
-    # メールサーバー
-    server = smtplib.SMTP(mail_host, mail_port)
+    try:
+        msg = MIMEText(msg, "plain", "utf-8")
+        msg["Subject"] = subject
+        msg["From"] = mail_from
+        msg["To"] = mail_to
 
-    #TLS認証
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
+        # メールサーバー
+        server = smtplib.SMTP(mail_host, mail_port)
 
-    #ログイン
-    server.login(mail_from, mail_pass)
+        try:
+            #TLS認証
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
 
-    # メールを送信する
-    server.send_message(msg)
-    
-    # 閉じる
-    server.quit()
+            #ログイン
+            server.login(mail_from, mail_pass)
+
+            # raise ValueError("エラーです") # @@Debug
+
+            # メール送信
+            server.send_message(msg)
+
+        except Exception as e:
+            raise e
+
+        finally:
+            # 閉じる
+            server.quit()
+
+    except Exception as e:
+        st.error("sendMail：" + str(e))
 
 # 合計人数設定
 def setTotal(num):
     global total_num
 
-    if num != 0: # 0以外？
-        # インクリメント
-        total_num += 1
-
-        # 補正
-        total_num = total_num % total_max
-        if total_num == 0:
+    try:
+        if num != 0: # 0以外？
+            # インクリメント
             total_num += 1
 
-    else: # リセット
-        total_num = 0
+            # 補正
+            total_num = total_num % total_max
+            if total_num == 0:
+                total_num += 1
 
-    # セッション保持
-    st.session_state[total_key] = total_num
+        else: # リセット
+            total_num = 0
+
+        # セッション保持
+        st.session_state[total_key] = total_num
+
+    except Exception as e:
+        st.error(e)
 
 # 物体抽出
 def extractionObject(cimage, detections):
-    num = 0 # 人数
-    (h, w) = cimage.shape[:2]
     objects = [] # 物体配列
+    num = 0 # 人数
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2] # 精度
+    try:
+        (h, w) = cimage.shape[:2]
 
-        if confidence > confidence_threshold: # 設定精度以上？
-            # extract the index of the class label from the `detections`,
-            # then compute the (x, y)-coordinates of the bounding box for
-            # the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2] # 精度
 
-            idx = int(detections[0, 0, i, 1])
-            ename = CLASSES_E[idx]
-            jname = CLASSES_J[idx]
+            if confidence > confidence_threshold: # 設定精度以上？
+                # extract the index of the class label from the `detections`,
+                # then compute the (x, y)-coordinates of the bounding box for
+                # the object
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-            if ename == "person": # 人間？
-                num += 1
-                col = color_red
-            else:
-                col = color_blue
+                idx = int(detections[0, 0, i, 1])
+                ename = CLASSES_E[idx]
+                jname = CLASSES_J[idx]
 
-            # 物体追加
-            objects.append((startX, startY, endX, endY, ename, jname, col, confidence))
+                if ename == "person": # 人間？
+                    num += 1
+                    col = color_red
+                else:
+                    col = color_blue
+
+                # 物体追加
+                objects.append((startX, startY, endX, endY, ename, jname, col, confidence))
+
+    except Exception as e:
+        pass
 
     return objects, num
 
@@ -221,56 +242,75 @@ def drawingResult(src, objects):
     # （参考）
     # https://note.com/npaka/n/nddb33be1b782
 
-    # 背景をPIL形式に変換
-    src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    pil_src = Image.fromarray(src)
-    draw = ImageDraw.Draw(pil_src)
+    try:
+        # 背景をPIL形式に変換
+        src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+        pil_src = Image.fromarray(src)
 
-    # 物体取得
-    for (startX, startY, endX, endY, ename, jname, col, confidence) in objects:
-        # ラベル
-        if plat in ("linux", "linux2"): # Linux？（日本語フォントなし）
-            label = ename # 英語
-        else:
-            label = jname # 日本語
+        try:
+            draw = ImageDraw.Draw(pil_src)
 
-        # ラベル描画
-        y = startY - (label_font_size+1) if startY - (label_font_size+1) > (label_font_size+1) else startY + (label_font_size+1)
-        draw.text(xy = (startX, y), text = label, fill = col, font = labelfont)
+            # 物体取得
+            for (startX, startY, endX, endY, ename, jname, col, confidence) in objects:
+                # ラベル
+                if plat in ("linux", "linux2"): # Linux？（日本語フォントなし）
+                    label = ename # 英語
+                else:
+                    label = jname # 日本語
 
-        # 枠描画
-        draw.rectangle([(startX, startY), (endX, endY)], outline=col, width=2)
+                # ラベル描画
+                y = startY - (label_font_size+1) if startY - (label_font_size+1) > (label_font_size+1) else startY + (label_font_size+1)
+                draw.text(xy = (startX, y), text = label, fill = col, font = labelfont)
 
-    # ロゴマークを合成
-    src_height, src_width = src.shape[:2]
-    logo_pos = (src_width - logo_width - logo_margin, logo_margin)
-    pil_src.paste(logo_pil, logo_pos, logo_pil)
+                # 枠描画
+                draw.rectangle([(startX, startY), (endX, endY)], outline=col, width=2)
 
-    # OpenCV形式に変換
-    return cv2.cvtColor(np.asarray(pil_src), cv2.COLOR_RGB2BGR)
+            # ロゴマークを合成
+            src_height, src_width = src.shape[:2]
+            logo_pos = (src_width - logo_width - logo_margin, logo_margin)
+            pil_src.paste(logo_pil, logo_pos, logo_pil)
+
+        except Exception as e:
+            pass
+
+        # OpenCV形式に変換
+        return cv2.cvtColor(np.asarray(pil_src), cv2.COLOR_RGB2BGR)
+
+    except Exception as e:
+        pass
 
 # コールバック処理
 def callback(frame: av.VideoFrame) -> av.VideoFrame:
-    # 画像変換
-    cimage = frame.to_ndarray(format="bgr24")
-    blob = cv2.dnn.blobFromImage(cv2.resize(cimage, (300, 300)), 0.007843, (300, 300), 127.5)
 
-    # 物体検出
-    net.setInput(blob)
-    detections = net.forward()
+    try:
+        # 画像変換
+        cimage = frame.to_ndarray(format="bgr24")
 
-    # 物体抽出
-    objects, num = extractionObject(cimage, detections)
+        try:
+            blob = cv2.dnn.blobFromImage(cv2.resize(cimage, (300, 300)), 0.007843, (300, 300), 127.5)
 
-    # 結果描画
-    cimage = drawingResult(cimage, objects)
+            # 物体検出
+            net.setInput(blob)
+            detections = net.forward()
 
-    # NOTE: This `recv` method is called in another thread,
-    # so it must be thread-safe.
-    # result_queue.put(result)  # TODO:
-    result_queue.put(num)  # TODO:
+            # 物体抽出
+            objects, num = extractionObject(cimage, detections)
 
-    return av.VideoFrame.from_ndarray(cimage, format="bgr24")
+            # 結果描画
+            cimage = drawingResult(cimage, objects)
+
+            # NOTE: This `recv` method is called in another thread,
+            # so it must be thread-safe.
+            # result_queue.put(result)  # TODO:
+            result_queue.put(num)  # TODO:
+
+        except Exception as e:
+            pass
+
+        return av.VideoFrame.from_ndarray(cimage, format="bgr24")
+
+    except Exception as e:
+        pass
 
 # メイン処理
 def appmain():
@@ -282,128 +322,132 @@ def appmain():
     global net
     global total_num
 
-    # ロガー
-    # logger = logging.getLogger(__name__)
+    try:
+        # ロガー
+        # logger = logging.getLogger(__name__)
 
-    # プラットフォーム
-    plat = sys.platform
+        # プラットフォーム
+        plat = sys.platform
 
-    # ファイルダウンロード
-    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564) # 学習モデル
-    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353) # プロトコル
+        # ファイルダウンロード
+        download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564) # 学習モデル
+        download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353) # プロトコル
 
-    # Session-specific caching
-    cache_key = "object_detection_dnn"
-    if cache_key in st.session_state:
-        net = st.session_state[cache_key]
-    else:
-        net = cv2.dnn.readNetFromCaffe(str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH))
-        st.session_state[cache_key] = net
+        # Session-specific caching
+        cache_key = "object_detection_dnn"
+        if cache_key in st.session_state:
+            net = st.session_state[cache_key]
+        else:
+            net = cv2.dnn.readNetFromCaffe(str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH))
+            st.session_state[cache_key] = net
 
-    # 合計人数取得
-    if total_key in st.session_state: # セッションあり？
-        # セッション取得
-        total_num = st.session_state[total_key]
-    else:
-        setTotal(0) # リセット
+        # 合計人数取得
+        if total_key in st.session_state: # セッションあり？
+            # セッション取得
+            total_num = st.session_state[total_key]
+        else:
+            setTotal(0) # リセット
 
-    # フォント
-    if plat == "win32": # Windows
-        font_name = font_name_win
-    if plat == "darwin": # Mac
-        font_name = font_name_mac
-    if plat in ("linux", "linux2"): # Linux
-        font_name = font_name_lnx
+        # フォント
+        if plat == "win32": # Windows
+            font_name = font_name_win
+        if plat == "darwin": # Mac
+            font_name = font_name_mac
+        if plat in ("linux", "linux2"): # Linux
+            font_name = font_name_lnx
 
-    # ラベルフォント
-    labelfont = ImageFont.truetype(font_name, label_font_size)
+        # ラベルフォント
+        labelfont = ImageFont.truetype(font_name, label_font_size)
 
-    # ロゴマーク読み込み
-    logo_image = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
-    logo_image = cv2.resize(logo_image, dsize=None, fx=logo_rate, fy=logo_rate)
-    logo_height, logo_width = logo_image.shape[:2]
-    # PIL形式に変換
-    logo_image = cv2.cvtColor(logo_image, cv2.COLOR_BGRA2RGBA)
-    logo_pil = Image.fromarray(logo_image)
+        # ロゴマーク読み込み
+        logo_image = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
+        logo_image = cv2.resize(logo_image, dsize=None, fx=logo_rate, fy=logo_rate)
+        logo_height, logo_width = logo_image.shape[:2]
+        # PIL形式に変換
+        logo_image = cv2.cvtColor(logo_image, cv2.COLOR_BGRA2RGBA)
+        logo_pil = Image.fromarray(logo_image)
 
-    # タイトル表示
-    st.subheader("みまもりくん")
+        # タイトル表示
+        st.subheader("みまもりくん ＜デモ＞")
 
-    # 状態表示
-    labels_placeholder = st.empty()
-    # 映像表示
-    streaming_placeholder = st.empty()
-    # スライダー表示
-    confidence_threshold = st.slider(
-        "精度", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
-    )
-
-    # メールアドレス
-    mailto_placeholder = st.text_input("宛先メールアドレス", "")
-    # メール送信
-    send_flag_placeholder = st.checkbox("人が見つかったときにメールを送信する")
-
-    # 映像表示
-    with streaming_placeholder.container():
-        # WEBカメラ
-        webrtc_ctx = webrtc_streamer(
-            key="object-detection",
-            mode=WebRtcMode.SENDRECV,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-            video_frame_callback=callback,
-            media_stream_constraints={"video": True, "audio": False},
-            async_processing=True,
-            translations={
-                "start": "開始",
-                "stop": "停止",
-                "select_device": "カメラ切替",
-                "media_api_not_available": "Media APIが利用できない環境です",
-                "device_ask_permission": "メディアデバイスへのアクセスを許可してください",
-                "device_not_available": "メディアデバイスを利用できません",
-                "device_access_denied": "メディアデバイスへのアクセスが拒否されました",
-            },
+        # 状態表示
+        labels_placeholder = st.empty()
+        # 映像表示
+        streaming_placeholder = st.empty()
+        # スライダー表示
+        confidence_threshold = st.slider(
+            "精度", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
         )
 
-    if webrtc_ctx.state.playing: # 映像配信中？
-        panels = labels_placeholder
+        # メールアドレス
+        mailto_placeholder = st.text_input("宛先メールアドレス", "")
+        # メール送信
+        send_flag_placeholder = st.checkbox("人が見つかったときにメールを送信する")
 
-        while webrtc_ctx.state.playing: # 配信中
-            try:
-                # キューの取得
-                nums = result_queue.get(timeout=1.0) # 人数取得
-            except queue.Empty:
-                nums = 0
+        # 映像表示
+        with streaming_placeholder.container():
+            # WEBカメラ
+            webrtc_ctx = webrtc_streamer(
+                key="object-detection",
+                mode=WebRtcMode.SENDRECV,
+                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+                video_frame_callback=callback,
+                media_stream_constraints={"video": True, "audio": False},
+                async_processing=True,
+                translations={
+                    "start": "開始",
+                    "stop": "停止",
+                    "select_device": "カメラ切替",
+                    "media_api_not_available": "Media APIが利用できない環境です",
+                    "device_ask_permission": "メディアデバイスへのアクセスを許可してください",
+                    "device_not_available": "メディアデバイスを利用できません",
+                    "device_access_denied": "メディアデバイスへのアクセスが拒否されました",
+                },
+            )
 
-            if nums > 0: # 人がいる？
-                panels.error("人を発見！")
+        if webrtc_ctx.state.playing: # 映像配信中？
+            panels = labels_placeholder
 
-                if total_num == 0: # 初回？
+            while webrtc_ctx.state.playing: # 配信中
+                try:
+                    # キューの取得
+                    nums = result_queue.get(timeout=1.0) # 人数取得
+                except queue.Empty:
+                    nums = 0
+
+                if nums > 0: # 人がいる？
+                    panels.error("人を発見！")
+
+                    if total_num == 0: # 初回？
+                        if send_flag_placeholder: # メール送信あり？
+                            if mailto_placeholder != "": # 送信メールアドレスあり？
+                                subject = "みまもりくん"
+                                msg = "人を発見しました。"
+                                #メール送信
+                                sendMail(mailto_placeholder, subject, msg)
+
                     if send_flag_placeholder: # メール送信あり？
-                        if mailto_placeholder != "": # 送信メールアドレスあり？
-                            subject = "みまもりくん"
-                            msg = "人を発見しました。"
-                            #メール送信
-                            sendMail(mailto_placeholder, subject, msg)
+                        # 合計人数更新
+                        setTotal(1)
+                    else:
+                        # 合計人数リセット
+                        setTotal(0)
 
-                if send_flag_placeholder: # メール送信あり？
-                    # 合計人数更新
-                    setTotal(1)
-                else:
-                    # 合計人数リセット
-                    setTotal(0)
+                else: # 人がいない
+                    panels.info("安全です")
 
-            else: # 人がいない
-                panels.info("安全です")
+                    # # 合計人数リセット
+                    # setTotal(0)
 
-                # # 合計人数リセット
-                # setTotal(0)
+            # 合計人数リセット
+            setTotal(0)
 
-        # 合計人数リセット
-        setTotal(0)
+        else: # 非配信中
+            # 合計人数リセット
+            setTotal(0)
 
-    else: # 非配信中
-        # 合計人数リセット
-        setTotal(0)
+    except Exception as e:
+        st.error("appmain：" + str(e))
 
 #コマンドプロンプト上で表示する
 if __name__ == "__main__":
